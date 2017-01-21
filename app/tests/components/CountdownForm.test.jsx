d49a1bff2d00297b3620e25b104ed898f186eee7
Vitalii Chernyshenko <Vitalii Chernyshenko>
2017-01-21 19:24:03 +0200

    Tests added to CountdownForm component

---
+++ app/tests/components/CountdownForm.test.jsx
@@ -0,0 +1,41 @@
+
+	var expect = require('expect');
+	var React = require('react');
+	var ReactDOM = require('react-dom');
+
+	var TestUtils = require('react-addons-test-utils');
+	var $ = require('jQuery');
+
+	var CountdownForm = require('CountdownForm');
+
+	describe('CountdownForm', () => {
+		it('should exist', () => {
+			expect(CountdownForm).toExist();
+		});
+
+		it('should call onSetCountdown if valid seconds entered', () => {
+			var spy = expect.createSpy();
+			var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy} />);
+			var $elem = $(ReactDOM.findDOMNode(countdownForm));
+
+			countdownForm.refs.seconds.value = '109';
+			TestUtils.Simulate.submit($elem[0]);
+
+			expect(spy).toHaveBeenCalledWith(109);
+
+
+		});
+
+		it('should not call onSetCountdown if invalid seconds entered', () => {
+			var spy = expect.createSpy();
+			var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy} />);
+			var $elem = $(ReactDOM.findDOMNode(countdownForm));
+
+			countdownForm.refs.seconds.value = '109b';
+			TestUtils.Simulate.submit($elem[0]);
+
+			expect(spy).toNotHaveBeenCalled();
+
+
+		});
+	});
